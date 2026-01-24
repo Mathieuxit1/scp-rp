273dc662045557f524f08d9e336873f617800586
Mathieuxit <tom.noirot21000@gmail.com>
2026-01-24 18:12:46 +0100

    modif du script.js

--- script.js
+++ script.js
@@ -2,21 +2,3 @@ function toggleMenu() {
     const menu = document.getElementById("navLinks");
     menu.classList.toggle("active");
 }
-
-<script>
-function filterSCP(type) {
-    const cards = document.querySelectorAll(".scp-card");
-
-    cards.forEach(card => {
-
-        if (type === "all") {
-            card.style.display = "block";
-        } else if (card.classList.contains(type)) {
-            card.style.display = "block";
-        } else {
-            card.style.display = "none";
-        }
-
-    });
-}
-</script>
